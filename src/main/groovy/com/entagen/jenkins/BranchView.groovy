835ad613bd78393693fe095234774e96ecb0a6ce
Ludovic Meurillon <ludovic.meurillon@orange.com>
2013-03-11 18:38:03 +0100

    Add a fixed limit to branch view name

--- src/main/groovy/com/entagen/jenkins/BranchView.groovy
+++ src/main/groovy/com/entagen/jenkins/BranchView.groovy
@@ -3,10 +3,15 @@ package com.entagen.jenkins
 class BranchView {
     String templateJobPrefix
     String branchName
+	int limit = 25
 
     public String getViewName() {
 		List nameParts = "$safeBranchName".tokenize('_');
-		return nameParts.get(nameParts.size-1);
+		String viewName = nameParts.get(nameParts.size-1);
+		if(viewName.length > limit){
+			viewName = viewName.subString(0,limit-3)+"...";
+		}
+		return viewName;
     }
 
     public String getSafeBranchName() {
